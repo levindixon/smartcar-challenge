004bf3e0d251ee990617fd4f097fd8d595dc2617
Levin Dixon <levindixon@gmail.com>
2015-11-03 01:44:03 -0800

    implement new stores, actions, and components

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
+
 import styles from './App.css';
 
+import AppActions from '../../actions/AppActions.js';
+import SchemaStore from '../../stores/SchemaStore.js';
+import LeftColumn from '../LeftColumn/LeftColumn.jsx';
+
+function getAppState() {
+  return {
+    schema: SchemaStore.getAll()
+  };
+}
+
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+
+  state = getAppState()
+
+  componentDidMount() {
+    SchemaStore.addChangeListener(this.onChange);
+    AppActions.getSchema();
   }
+
+  componentWillUnmount() {
+    SchemaStore.removeChangeListener(this.onChange);
+  }
+
+  onChange = () => {
+    this.setState(getAppState());
+  }
+
   render() {
     return (
-      <div className={styles.app}></div>
+      <div className={styles.app}>
+        <LeftColumn schema={this.state.schema} />
+      </div>
     );
   }
 }
