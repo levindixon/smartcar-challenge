13bd03b6681177d049488274dfabd3558733b373
Levin Dixon <levindixon@gmail.com>
2015-11-04 03:27:09 -0800

    style docs

--- app/components/Docs/Docs.jsx
+++ app/components/Docs/Docs.jsx
@@ -17,7 +17,25 @@ export default class Docs extends Component {
 
   render() {
     return (
-      <div className={styles.contents}>
+      <div className={styles.docs}>
+        <div
+          className={styles.docItem}
+          id="introduction"
+        >
+          <div className={styles.content}>
+            <h2 className={styles.title}>Introduction</h2>
+            <p className={styles.paragraph}>
+              Welcome to the smartcar api docs!
+            </p>
+            <p className={styles.paragraph}>
+              We’ve gone ahead and added your API key into each example,
+              along with an example vehicle ID. Feel free to generate
+              a new API key or change your vehicle ID at anytime and we’ll
+              update the examples for you.
+            </p>
+          </div>
+          <div className={styles.example}></div>
+        </div>
         {this.props.schema.map((item, i) => {
           return (
             <DocsItem
