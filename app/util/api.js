bfcbe9ef3f1aa1eec52f03f5ba4ccd9dd462d1d6
Levin Dixon <levindixon@gmail.com>
2015-11-03 22:09:00 -0800

    add apiKey and vehicleId gets to api utility

--- app/util/api.js
+++ app/util/api.js
@@ -5,5 +5,18 @@ export default {
     return new Promise((resolve) => {
       resolve(schema);
     });
+  },
+
+  getApiKey() {
+    return new Promise((resolve) => {
+      let apiKey = Math.random().toString(36).substring(5);
+      resolve(apiKey);
+    });
+  },
+
+  getVehicleId() {
+    return new Promise((resolve) => {
+      resolve('1234');
+    });
   }
 };
